db785cee04dbe47ac85e2c21098c27733a191af2
Benjamin Schneider <bschneider97@t-online.de>
2020-04-02 22:27:31 +0200

    First commit (obysouly not)

---
+++ azuracast-widget.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ *  * Plugin Name: Now playing for AzuraCast
+ *  * Description: Shows in a widget what is currently being played on the AzuraCast instance.
+ *  * Plugin URI: https://javik.net/azuracast-widget
+ *  * Version: 2.0
+ *  * Author: Javik
+ *  * Author URI: https://javik.net
+ *  * Text Domain: now-playing-widget-fuer-azuracast-stationen
+ *  * Domain Path: /languages
+ *   */
